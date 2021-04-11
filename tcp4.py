c4437677ca041b8d81d3989238e709d9e9ea35cc
yhtham1 <yhtham1@m1.pxaa.net>
2021-04-11 09:47:30 +0900

    from pycharm

--- tcp4.py
+++ tcp4.py
@@ -19,20 +19,22 @@ import threading
 import socketserver
 from queue import Queue
 
-rxq=Queue()			#各クライアントからの入力をバッファリングする。 lgetc()で読み出す
-FD_LIST = []		#ここに接続変数を追加、削除する。接続数分存在する。lprintfで出力する。
-mon_tcp = 0			#送受信のデバック
-use_binary=False	#バイナリーデータでのやり取り True:False
+rxq = Queue()  # 各クライアントからの入力をバッファリングする。 lgetc()で読み出す
+FD_LIST = []  # ここに接続変数を追加、削除する。接続数分存在する。lprintfで出力する。
+mon_tcp = 0  # 送受信のデバック
+use_binary = False  # バイナリーデータでのやり取り True:False
 
-def lgetc():		#tcp接続先から入力する。
+
+def lgetc():  # tcp接続先から入力する。
 	ans = ''
-	if False==rxq.empty():
+	if False == rxq.empty():
 		ans = rxq.get()
 		return ans
 	return None
 
-def lprintf(msg):	#tcp接続先へ出力する。
-#	print(msg)
+
+def lprintf(msg):  # tcp接続先へ出力する。
+	#	print(msg)
 	for req in FD_LIST:
 		try:
 			if True == use_binary:
@@ -47,41 +49,44 @@ def PushRXData(str):
 	for c in str:
 		rxq.put(c)
 
+
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 	def handle(self):
 		thread_name = threading.current_thread().getName()
 		prefix = '[{}]'.format(thread_name)
 
-		r_ip,r_port = self.request.getpeername()
+		r_ip, r_port = self.request.getpeername()
 
-#		print('{} -- connected'.format(self.request.getpeername()))
+		#		print('{} -- connected'.format(self.request.getpeername()))
 		print('IP:{} -- connected'.format(r_ip))
-#		print(dir(self.request))
-#		print(self.request.getpeername())
+		#		print(dir(self.request))
+		#		print(self.request.getpeername())
 		FD_LIST.append(self.request)
 		while True:
 			try:
 				dat = self.request.recv(1024)
 			except:
 				break
-			if 0 == len(dat):	#データ長が0なら切断済み
+			if 0 == len(dat):  # データ長が0なら切断済み
 				break
 			if mon_tcp:
 				print('{} -- RX[{}]'.format(prefix, dat))
 			if True == use_binary:
-				rxq.put(dat) 			#読んだ文字をキューに積む[teratermのenter入力は '\r\0'なので注意]
+				rxq.put(dat)  # 読んだ文字をキューに積む[teratermのenter入力は '\r\0'なので注意]
 			else:
 				c1 = dat.decode('utf-8')
-				PushRXData(c1) 			#読んだ文字をキューに積む[teratermのenter入力は '\r\0'なので注意]
+				PushRXData(c1)  # 読んだ文字をキューに積む[teratermのenter入力は '\r\0'なので注意]
 		print('IP:{} -- disconnect'.format(r_ip))
 		FD_LIST.remove(self.request)
 		print('FD_LIST is {}'.format(len(FD_LIST)))
 
+
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 	pass
 
+
 def get_ip(server_ip):
-	#server_ipに接続してそのときのローカルＩＰを得る
+	# server_ipに接続してそのときのローカルＩＰを得る
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	try:
 		# doesn't even have to be reachable
@@ -93,8 +98,9 @@ def get_ip(server_ip):
 		s.close()
 	return IP
 
+
 def start(host, port):
-	#func
+	# func
 	FD_LIST.clear()
 	server = ThreadedTCPServer((host, port), ThreadedTCPRequestHandler)
 	server.allow_reuse_address = True
@@ -103,11 +109,12 @@ def start(host, port):
 	server_thread.start()
 	print("TCP4 telnetサーバ tcp4.py listen:{}".format(port))
 
+
 if __name__ == "__main__":
-	ip = get_ip( '192.168.0.3' )
+	ip = get_ip('192.168.0.3')
 	print('my ip is :{}'.format(ip))
 	mon_tcp = 1
-	start('0.0.0.0', 5000 )
+	start('0.0.0.0', 5000)
 	while True:
 		time.sleep(1)
 	server.shutdown()
